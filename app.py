caa334905a1a3a450c5e68f7251d562109ca4e77
UnclrHoward <78525917+UnclrHoward@users.noreply.github.com>
2025-02-09 16:19:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,12 +31,9 @@ if st.button("Generate Collage"):
         start_time = time.time()
         # Fetch top 100 tracks
         top_tracks = []
+        st.write("FUCK THIS")
         for offset in [0, 50]:
-            try:
     tracks = sp.current_user_top_tracks(limit=50, time_range='long_term', offset=offset)
-except Exception as e:
-    st.error(f"Ошибка при получении треков: {e}")
-    break  # если это внутри цикла, иначе используйте подходящий способ выхода
             top_tracks.extend(tracks['items'])
             end_time = time.time()
             print(f"Fetched tracks in {end_time - start_time:.2f} seconds")
